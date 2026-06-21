f502a5965ddb2306232d444382d8106883043369
chidi esobe <chidiesobe@gmail.com>
2026-06-21 14:45:49 +0100

    chore: bump version to v0.1.8

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ async def lifespan(_app: FastAPI) -> AsyncIterator[None]:
 
 app = FastAPI(
     title=settings.app_name,
-    version="0.1.7",
+    version="0.1.8",
     docs_url=None,
     redoc_url=None,
     openapi_url=None,
